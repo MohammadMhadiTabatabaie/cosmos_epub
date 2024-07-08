783e8d96986dbee3ff7627bb79b622a0ff3e982f
MohammadMhadiTabatabaie <tabatabaiemahdi708@gmail.com>
2024-07-08 11:24:42 +0330

    Creating epub pages

--- lib/Helpers/pagination.dart
+++ lib/Helpers/pagination.dart
@@ -59,7 +59,8 @@ class _PagingWidgetState extends State<PagingWidget> {
   final _pageController = GlobalKey<PageFlipWidgetState>();
   int _pagesLoaded = 0;
   //static const int _pagesBatchSize = 10;
-  final int _pagesBatchSize = 10;
+  final int _pagesBatchSize = 100;
+  late double _pageHeight;
   // int _pagesLoaded = 0;
   @override
   void initState() {
@@ -70,7 +71,16 @@ class _PagingWidgetState extends State<PagingWidget> {
     // widget.handlerCallback(handler); // callback call.
     // super.initState();
     super.initState();
-    _loadMorePages(initialLoad: true);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadMorePages(initialLoad: true);
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _pageHeight = MediaQuery.of(context).size.height -
+        34.0; // تنظیم ارتفاع صفحه با توجه به اندازه دستگاه
   }
 
   void _loadMorePages({required bool initialLoad}) async {
@@ -245,31 +255,134 @@ class _PagingWidgetState extends State<PagingWidget> {
   }
 */
   Future<List<String>> _paginate(int pagesToLoad) async {
-    // محاسبه صفحات بر اساس تعداد کاراکترها یا اندازه متن و صفحه
-    // در اینجا فرض می‌کنیم متن را به قسمت‌های ۱۰ صفحه‌ای تقسیم می‌کنیم
-    final textLength = widget.textContent.length;
-    final pageSize = (textLength / _pagesBatchSize).ceil();
+    //   final page_Size = _initializedRenderBox.size;
+    //   _pageTexts.clear();
+    //   // محاسبه اندازه صفحه
+    //   final textPainter = TextPainter(
+    //     textDirection: TextDirection.rtl,
+    //     text: TextSpan(
+    //       text: widget.textContent,
+    //       style: widget.style,
+    //     ),
+    //   );
+    //   textPainter.layout(
+    //     minWidth: 0,
+    //     maxWidth: page_Size.width,
+    //   );
+    //   final textSpan = TextSpan(
+    //     text: widget.textContent,
+    //     style: widget.style,
+    //   );
+    //   double currentPageBottom = page_Size.height;
+    //   int currentPageStartIndex = 0;
+    //   int currentPageEndIndex = 0;
+    //   // final left = line.left;
+    //   // final top = line.baseline - line.ascent;
+    //   // final bottom = line.baseline + line.descent;
+    //   var innerHtml = widget.innerHtmlContent;
+
+    //   currentPageStartIndex = currentPageEndIndex;
+    //   currentPageBottom =
+    //       //top +
+    //        page_Size.height - (innerHtml != null ? 200.h : 100.h);
+    //   // final textHeight = textPainter.size.height;
+    //   // final pageHeight = MediaQuery.of(context).size.height;
+    //   // final linesPerPage =
+    //   //     (pageHeight / textHeight * textPainter.computeLineMetrics().length)
+    //   //         .ceil();
+
+    //   //final textHeight = textPainter.size.height;
+    //   // final linesPerPage = (_pageHeight / textHeight * textPainter.computeLineMetrics().length).ceil();
+    //   // محاسبه صفحات بر اساس تعداد کاراکترها یا اندازه متن و صفحه
+    //   // در اینجا فرض می‌کنیم متن را به قسمت‌های ۱۰ صفحه‌ای تقسیم می‌کنیم
+    //   final textLength = widget.textContent.length;
+    //   final pageSize = (textLength / _pagesBatchSize).ceil();
+    //   List<String> newPages = [];
+    //   for (int i = 0; i < pagesToLoad && i < _pagesBatchSize; i++) {
+    //     final start = i * pageSize;
+    //     final end = (i + 1) * pageSize;
+    //     final pageContent = widget.textContent
+    //         .substring(start.toInt(), end.toInt().clamp(0, textLength));
+    //     newPages.add(pageContent);
+    //   }
+    //   return newPages;
+    // }
+    _pageTexts.clear();
     List<String> newPages = [];
-    for (int i = 0; i < pagesToLoad && i < _pagesBatchSize; i++) {
-      final start = i * pageSize;
-      final end = (i + 1) * pageSize;
-      final pageContent = widget.textContent
-          .substring(start.toInt(), end.toInt().clamp(0, textLength));
+
+    final textSpan = TextSpan(
+      text: widget.textContent,
+      style: widget.style,
+    );
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.rtl,
+    );
+
+    // Lay out the textPainter with a max width constraint
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: MediaQuery.of(context).size.width - 32.0,
+    );
+
+    final lines = textPainter.computeLineMetrics();
+    int currentLine = 0;
+    while (currentLine < lines.length && newPages.length < pagesToLoad) {
+      int start = textPainter
+          .getPositionForOffset(Offset(0, lines[currentLine].baseline))
+          .offset;
+      int endLine = currentLine;
+
+      while (endLine < lines.length &&
+          lines[endLine].baseline < lines[currentLine].baseline + _pageHeight) {
+        endLine++;
+      }
+
+      int end = textPainter
+          .getPositionForOffset(Offset(0, lines[endLine - 1].baseline))
+          .offset;
+      final pageContent = widget.textContent.substring(start, end);
       newPages.add(pageContent);
+      currentLine = endLine;
     }
+
     return newPages;
   }
 
   List<Widget> _buildPageWidgets(List<String> pageTexts) {
     return pageTexts.map((text) {
-      return GestureDetector(
-        onTap: widget.onTextTap,
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            text,
-            style: widget.style,
-            textAlign: TextAlign.justify,
+      return SingleChildScrollView(
+        child: GestureDetector(
+          onTap: widget.onTextTap,
+          child: Container(
+            height: _pageHeight,
+            padding: const EdgeInsets.all(16.0),
+            child: widget.innerHtmlContent != null
+                ? HtmlWidget(
+                    text,
+                    customStylesBuilder: (element) {
+                      return {
+                        'text-align': 'justify',
+                      };
+                    },
+                    onTapUrl: (String? s) async {
+                      if (s != null && s == "a") {
+                        if (s.contains("chapter")) {
+                          setState(() {
+                            // Write logic for goto chapter
+                          });
+                        }
+                      }
+                      return true;
+                    },
+                    textStyle: widget.style,
+                  )
+                : Text(
+                    text,
+                    style: widget.style,
+                    textAlign: TextAlign.justify,
+                  ),
           ),
         ),
       );
@@ -337,7 +450,8 @@ class _PagingWidgetState extends State<PagingWidget> {
                                 isRightSwipe: true,
                                 initialIndex: 0,
                                 children: pages,
-                              //  backgroundColor: widget.style.backgroundColor!,
+                                backgroundColor:
+                                    widget.style.backgroundColor ?? Colors.red,
                                 onPageFlip: (index) {
                                   setState(() {
                                     _currentPageIndex = index;
@@ -352,60 +466,62 @@ class _PagingWidgetState extends State<PagingWidget> {
                                 },
                               )),
                         ),
-                        Visibility(
-                          visible: false,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.first_page),
-                                onPressed: () {
-                                  setState(() {
-                                    _currentPageIndex = 0;
-                                    _pageController.currentState
-                                        ?.goToPage(_currentPageIndex);
-                                  });
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.navigate_before),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_currentPageIndex > 0)
-                                      _currentPageIndex--;
-                                    _pageController.currentState
-                                        ?.goToPage(_currentPageIndex);
-                                  });
-                                },
-                              ),
-                              Text(
-                                '${_currentPageIndex + 1}/${_pageTexts.length}',
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.navigate_next),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_currentPageIndex <
-                                        _pageTexts.length - 1)
-                                      _currentPageIndex++;
-                                    _pageController.currentState
-                                        ?.goToPage(_currentPageIndex);
-                                  });
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.last_page),
-                                onPressed: () {
-                                  setState(() {
-                                    _currentPageIndex = _pageTexts.length - 1;
-                                    _pageController.currentState
-                                        ?.goToPage(_currentPageIndex);
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
+
+                        //  برای باتم هست که اسم فصل ها و جابه جای
+                        // Visibility(
+                        //   visible: false,
+                        //   child: Row(
+                        //     mainAxisAlignment: MainAxisAlignment.center,
+                        //     children: [
+                        //       IconButton(
+                        //         icon: Icon(Icons.first_page),
+                        //         onPressed: () {
+                        //           setState(() {
+                        //             _currentPageIndex = 0;
+                        //             _pageController.currentState
+                        //                 ?.goToPage(_currentPageIndex);
+                        //           });
+                        //         },
+                        //       ),
+                        //       IconButton(
+                        //         icon: Icon(Icons.navigate_before),
+                        //         onPressed: () {
+                        //           setState(() {
+                        //             if (_currentPageIndex > 0)
+                        //               _currentPageIndex--;
+                        //             _pageController.currentState
+                        //                 ?.goToPage(_currentPageIndex);
+                        //           });
+                        //         },
+                        //       ),
+                        //       Text(
+                        //         '${_currentPageIndex + 1}/${_pageTexts.length}',
+                        //       ),
+                        //       IconButton(
+                        //         icon: Icon(Icons.navigate_next),
+                        //         onPressed: () {
+                        //           setState(() {
+                        //             if (_currentPageIndex <
+                        //                 _pageTexts.length - 1)
+                        //               _currentPageIndex++;
+                        //             _pageController.currentState
+                        //                 ?.goToPage(_currentPageIndex);
+                        //           });
+                        //         },
+                        //       ),
+                        //       IconButton(
+                        //         icon: Icon(Icons.last_page),
+                        //         onPressed: () {
+                        //           setState(() {
+                        //             _currentPageIndex = _pageTexts.length - 1;
+                        //             _pageController.currentState
+                        //                 ?.goToPage(_currentPageIndex);
+                        //           });
+                        //         },
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
                       ],
                     ),
                   ],
